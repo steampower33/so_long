51627f6e8a30bacef4d22587e265d066ad5c0e83
SeungMinLee <seunlee2@student.42seoul.kr>
2023-08-11 19:50:47 +0900

    delete libft

--- so_long.c
+++ so_long.c
@@ -6,13 +6,22 @@
 /*   By: seunlee2 <seunlee2@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/08 17:28:58 by seunlee2          #+#    #+#             */
-/*   Updated: 2023/08/08 19:21:32 by seunlee2         ###   ########.fr       */
+/*   Updated: 2023/08/11 19:44:09 by seunlee2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 
-int	main(void)
+void	ft_error(char *str, int code)
 {
-	
+	perror(str);
+	exit(code);
+}
+
+int	main(int argc, char **argv)
+{
+	if (argc != 2)
+		ft_error("Error\n", 1);
+	// if (ft_strnstr())
+	printf("%s\n", argv[1] + ft_strlen(argv[1]) - 4);
 }
--- so_long.h
+++ so_long.h
@@ -6,11 +6,17 @@
 /*   By: seunlee2 <seunlee2@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/08 17:28:54 by seunlee2          #+#    #+#             */
-/*   Updated: 2023/08/08 17:51:37 by seunlee2         ###   ########.fr       */
+/*   Updated: 2023/08/11 19:37:35 by seunlee2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifdef SO_LONG_H
+#ifndef SO_LONG_H
 # define SO_LONG_H
+
+# include "libft.h"
+# include <fcntl.h>
+# include <stdio.h>
+
+void	ft_error(char *str, int code);
 
 #endif
